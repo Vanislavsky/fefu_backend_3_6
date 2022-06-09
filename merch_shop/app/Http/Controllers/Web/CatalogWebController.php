bb521a7d18ca4bb563f544749339a3a3d852bbcc
Vanislavsky <serzh7214@mail,ru>
2022-06-10 04:14:49 +1000

    Lesson 9: Add search, sorting, filters to catalog (web)

--- merch_shop/app/Http/Controllers/Web/CatalogWebController.php
+++ merch_shop/app/Http/Controllers/Web/CatalogWebController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Web;
 
+use App\Http\Filters\ProductFilter;
+use App\Http\Requests\CatalogFormRequest;
 use App\Http\Requests\StoreProductCategoryRequest;
 use App\Http\Requests\UpdateProductCategoryRequest;
 use App\Models\Product;
@@ -14,8 +16,9 @@ class CatalogWebController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(string $slug = null)
+        public function index(CatalogFormRequest $request, string $slug = null)
     {
+        $requestData = $request->validated();
         $query = ProductCategory::query()->with('children', 'products');
 
         if ($slug === null) {
@@ -26,12 +29,27 @@ class CatalogWebController extends Controller
 
         $categories = $query->get();
         try {
-            $products = ProductCategory::getTreeProductsBuilder($categories)
-                ->orderBy('id')
-                ->paginate();
+            $products = ProductCategory::getTreeProductsBuilder($categories);
         } catch (\Exception $exception) {
             abort(422, $exception->getMessage());
         }
-        return view('catalog.catalog', ['categories' => $categories, 'products' => $products]);
+
+        $filters = ProductFilter::build($products, $requestData['filters'] ?? []);
+        ProductFilter::apply($products, $requestData['filters'] ?? []);
+
+        if (isset($requestData['search_query'])) {
+            $products->search($requestData['search_query']);
+        }
+
+        $sortMode = $requestData['sort_mode'] ?? null;
+        if ($sortMode === 'price_asc') {
+            $products->orderBy('price');
+        } else if ($sortMode === 'price_desc') {
+            $products->orderBy('price', 'desc');
+        }
+
+        return view('catalog.catalog', ['categories' => $categories,
+            'products' => $products->orderBy('products.id')->paginate(),
+            'filters' => $filters]);
     }
 }
--- merch_shop/resources/views/catalog/catalog.blade.php
+++ merch_shop/resources/views/catalog/catalog.blade.php
@@ -8,6 +8,34 @@
 </head>
 <body>
 <div>
+    <form method="GET">
+        <div>
+            <label for="search_query">Search</label>
+            <input type="text" name="search_query" id="search_query" value="{{ request('search_query') }}">
+        </div>
+        <div>
+            <label for="sort_mode">Sort mode</label>
+            <select name="sort_mode" id="sort_mode">
+                <option value="price_asc">price asc</option>
+                <option value="price_desc">price desc</option>
+            </select>
+        </div>
+        @foreach($filters as $filter)
+            <div>
+                <h4>{{ $filter->name }}</h4>
+                @foreach($filter->options as $option)
+                    <div>
+                        <label>
+                            <input type="checkbox" value="{{ $option->value }}"
+                                   name="filters[{{ $filter->key }}][]" {{ $option->isSelected ? 'checked' : '' }}>
+                            {{ $option->value }} ({{ $option->productCount }} )
+                        </label>
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
+        <button>Apply</button>
+    </form>
     Каталог
     @include('catalog.catalog_list', ['categories', $categories])
 
